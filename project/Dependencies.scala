f460f3c946e4899677a2d3870018f41eaedb0323
Muhammet Orazov <m.orazow@gmail.com>
2018-10-22 14:26:14 +0200

    Update versions. Use hadoop 2.8.4 and parquet 1.8.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,8 +14,10 @@ object Dependencies {
     "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0",
     "com.exasol" % "exasol-jdbc" % "6.0.8",
     "com.exasol" % "exasol-script-api" % "6.0.8",
-    "org.apache.hadoop" % "hadoop-aws" % "3.1.1",
-    "org.apache.hadoop" % "hadoop-common" % "3.1.1"
+    "org.apache.hadoop" % "hadoop-aws" % "2.8.4",
+    "org.apache.hadoop" % "hadoop-common" % "2.8.4" exclude ("org.slf4j", "slf4j-log4j12"),
+    "org.apache.hadoop" % "hadoop-hdfs" % "2.8.4",
+    "org.apache.parquet" % "parquet-avro" % "1.8.1"
   )
 
   /** Test dependencies only required in `test` */
