6784d0361a10bfe0219b00a65dfa6c8452f4d25b
Muhammet Orazov <m.orazow@gmail.com>
2019-05-06 16:35:34 +0200

    Fix typo in parquet writer validation parameter.

--- src/main/scala/com/exasol/cloudetl/parquet/ParquetWriteOptions.scala
+++ src/main/scala/com/exasol/cloudetl/parquet/ParquetWriteOptions.scala
@@ -27,7 +27,7 @@ object ParquetWriteOptions {
       params.get("PARQUET_BLOCK_SIZE").fold(ParquetWriter.DEFAULT_BLOCK_SIZE)(_.toInt)
     val pageSize = params.get("PARQUET_PAGE_SIZE").fold(ParquetWriter.DEFAULT_PAGE_SIZE)(_.toInt)
     val dictionary = params.get("PARQUET_DICTIONARY_ENCODING").fold(true)(_.toBoolean)
-    val validation = params.get("PARQUET_VALIDAIONT").fold(true)(_.toBoolean)
+    val validation = params.get("PARQUET_VALIDATION").fold(true)(_.toBoolean)
 
     ParquetWriteOptions(blockSize, pageSize, compressionCodec, dictionary, validation)
   }
