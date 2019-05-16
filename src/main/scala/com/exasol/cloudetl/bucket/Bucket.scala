b849ad43abd3fdf3df31787437189c0e23656b4a
Muhammet Orazov <m.orazow@gmail.com>
2019-05-16 10:09:52 +0200

    Make bucket fileSystem field final.

--- src/main/scala/com/exasol/cloudetl/bucket/Bucket.scala
+++ src/main/scala/com/exasol/cloudetl/bucket/Bucket.scala
@@ -39,7 +39,7 @@ abstract class Bucket {
    * The Hadoop [[org.apache.hadoop.fs.FileSystem]] for this specific
    * bucket path.
    */
-  lazy val fileSystem: FileSystem =
+  final lazy val fileSystem: FileSystem =
     FileSystem.get(new URI(bucketPath), getConfiguration())
 
   /**
@@ -87,7 +87,7 @@ object Bucket extends LazyLogging {
 
   /**
    * An apply method that creates different [[Bucket]] classes depending
-   * on the path schema.
+   * on the path scheme.
    *
    * @param params The key value parameters
    * @return A [[Bucket]] class for the given path
