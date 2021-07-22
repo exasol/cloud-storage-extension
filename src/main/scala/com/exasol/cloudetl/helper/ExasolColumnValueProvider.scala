0ee256fd54a6f33e616478852f6b041e36121c0b
Muhammet Orazov <m.orazow@gmail.com>
2021-07-22 13:16:29 +0200

    Apply suggestions from code review
    Co-authored-by: Anastasiia Sergienko <46891819+AnastasiiaSergienko@users.noreply.github.com>

--- src/main/scala/com/exasol/cloudetl/helper/ExasolColumnValueProvider.scala
+++ src/main/scala/com/exasol/cloudetl/helper/ExasolColumnValueProvider.scala
@@ -4,13 +4,13 @@ import com.exasol.ExaIterator
 import com.exasol.cloudetl.data.ExaColumnInfo
 
 /**
- * A class that return column value given index and type from Exasol iterator.
+ * A class that returns column value on a given index and type from Exasol iterator.
  */
 final case class ExasolColumnValueProvider(iterator: ExaIterator) extends JavaClassTypes {
 
   /**
    * Returns a value from Exasol [[ExaIterator]] iterator on given index
-   * which have [[com.exasol.cloudetl.data.ExaColumnInfo]] column type.
+   * which has [[com.exasol.cloudetl.data.ExaColumnInfo]] column type.
    *
    * @param index index of a column
    * @param columnInfo column information that holds column type
