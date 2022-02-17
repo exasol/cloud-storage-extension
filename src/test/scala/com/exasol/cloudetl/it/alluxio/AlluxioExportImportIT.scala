14d9cc33180d0388c04305fb2926f4ffb20c5cc9
morazow <m.orazow@gmail.com>
2022-02-17 11:18:39 +0100

    Updated alluxio version

--- src/test/scala/com/exasol/cloudetl/it/alluxio/AlluxioExportImportIT.scala
+++ src/test/scala/com/exasol/cloudetl/it/alluxio/AlluxioExportImportIT.scala
@@ -12,7 +12,7 @@ import org.hamcrest.MatcherAssert.assertThat
 
 class AlluxioExportImportIT extends BaseIntegrationTest {
 
-  val ALLUXIO_IMAGE = "alluxio/alluxio:2.7.2"
+  val ALLUXIO_IMAGE = "alluxio/alluxio:2.7.3"
   val SCHEMA_NAME = "ALLUXIO_SCHEMA"
 
   val alluxioMainContainer =
