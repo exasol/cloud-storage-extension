ce74b958be8a6cb11b5f8691fc4f429eaaf05e54
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-10-11 10:34:28 +0200

    Add integration test

--- src/test/java/com/exasol/cloudetl/extension/ExtensionIT.java
+++ src/test/java/com/exasol/cloudetl/extension/ExtensionIT.java
@@ -1,42 +1,60 @@
 package com.exasol.cloudetl.extension;
 
 import java.io.FileNotFoundException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.concurrent.TimeoutException;
 
+import org.junit.jupiter.api.*;
+
 import com.exasol.bucketfs.BucketAccessException;
+import com.exasol.exasoltestsetup.ExasolTestSetup;
+import com.exasol.exasoltestsetup.ExasolTestSetupFactory;
+import com.exasol.extensionmanager.itest.ExtensionManagerSetup;
+import com.exasol.extensionmanager.itest.builder.ExtensionBuilder;
+import com.exasol.mavenprojectversiongetter.MavenProjectVersionGetter;
+
+import junit.framework.AssertionFailedError;
 
 class ExtensionIT {
     private static final Path EXTENSION_SOURCE_DIR = Paths.get("extension").toAbsolutePath();
-    private static final int EXPECTED_PARAMETER_COUNT = 10;
+    private static final String PROJECT_VERSION = MavenProjectVersionGetter.getCurrentProjectVersion();
+    private static final Path ADAPTER_JAR = getAdapterJar(PROJECT_VERSION);
     private static ExasolTestSetup exasolTestSetup;
     private static ExtensionManagerSetup setup;
-    private static String s3BucketName;
-    private static String projectVersion;
 
     @BeforeAll
     static void setup() throws FileNotFoundException, BucketAccessException, TimeoutException {
-        exasolTestSetup = new ExasolTestSetupFactory(IntegrationTestSetup.CLOUD_SETUP_CONFIG).getTestSetup();
+        exasolTestSetup = new ExasolTestSetupFactory(Paths.get("no-cloud-setup")).getTestSetup();
         setup = ExtensionManagerSetup.create(exasolTestSetup, ExtensionBuilder.createDefaultNpmBuilder(
                 EXTENSION_SOURCE_DIR, EXTENSION_SOURCE_DIR.resolve("dist/s3-vs-extension.js")));
-        s3TestSetup = new AwsS3TestSetup();
-        projectVersion = MavenProjectVersionGetter.getCurrentProjectVersion();
-        exasolTestSetup.getDefaultBucket().uploadFile(IntegrationTestSetup.ADAPTER_JAR_LOCAL_PATH,
-                IntegrationTestSetup.ADAPTER_JAR);
+        exasolTestSetup.getDefaultBucket().uploadFile(ADAPTER_JAR, ADAPTER_JAR.getFileName().toString());
+    }
+
+    private static Path getAdapterJar(final String projectVersion) {
+        final Path jar = Paths.get("target").resolve("exasol-cloud-storage-extension-" + projectVersion+ ".jar")
+                .toAbsolutePath();
+        if (Files.exists(jar)) {
+            return jar;
+        } else {
+            throw new AssertionFailedError("Adapter jar " + jar + " does not exist. Run mvn package.");
+        }
     }
 
     @AfterAll
-    static void teardown()  {
+    static void teardown() throws Exception {
         if (setup != null) {
             setup.close();
         }
-        exasolTestSetup.getDefaultBucket().deleteFileNonBlocking(IntegrationTestSetup.ADAPTER_JAR);
+        exasolTestSetup.getDefaultBucket().deleteFileNonBlocking(ADAPTER_JAR.getFileName().toString());
         exasolTestSetup.close();
     }
 
     @AfterEach
     void cleanup() {
         setup.cleanup();
+    }
+
+    @Test void test(){
+
     }
 }
