ab04532914fbaa9de8e68abc7f38ff2a1c8de420
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-10-12 15:51:22 +0200

    Add integration tests for running import/export scripts

--- src/test/java/com/exasol/cloudetl/extension/ExtensionIT.java
+++ src/test/java/com/exasol/cloudetl/extension/ExtensionIT.java
@@ -7,34 +7,53 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.io.FileNotFoundException;
 import java.nio.file.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Logger;
 
 import org.junit.jupiter.api.*;
 
 import com.exasol.bucketfs.BucketAccessException;
+import com.exasol.dbbuilder.dialects.Table;
+import com.exasol.dbbuilder.dialects.exasol.*;
 import com.exasol.exasoltestsetup.ExasolTestSetup;
 import com.exasol.exasoltestsetup.ExasolTestSetupFactory;
 import com.exasol.extensionmanager.client.model.ExtensionsResponseExtension;
+import com.exasol.extensionmanager.itest.ExtensionManagerClient;
 import com.exasol.extensionmanager.itest.ExtensionManagerSetup;
 import com.exasol.extensionmanager.itest.builder.ExtensionBuilder;
+import com.exasol.matcher.TypeMatchMode;
 import com.exasol.mavenprojectversiongetter.MavenProjectVersionGetter;
 
 import junit.framework.AssertionFailedError;
 
 class ExtensionIT {
+    private static final Logger LOGGER = Logger.getLogger(ExtensionIT.class.getName());
     private static final Path EXTENSION_SOURCE_DIR = Paths.get("extension").toAbsolutePath();
     private static final String PROJECT_VERSION = MavenProjectVersionGetter.getCurrentProjectVersion();
+    private static final String S3_CONNECTION = "S3_CONNECTION";
     private static final Path ADAPTER_JAR = getAdapterJar();
+
     private static ExasolTestSetup exasolTestSetup;
     private static ExtensionManagerSetup setup;
+    private static ExtensionManagerClient client;
+    private static S3Setup s3setup;
+    private static Connection connection;
+    private static ExasolObjectFactory exasolObjectFactory;
 
     @BeforeAll
-    static void setup() throws FileNotFoundException, BucketAccessException, TimeoutException {
+    static void setup() throws FileNotFoundException, BucketAccessException, TimeoutException, SQLException {
         exasolTestSetup = new ExasolTestSetupFactory(Paths.get("no-cloud-setup")).getTestSetup();
         setup = ExtensionManagerSetup.create(exasolTestSetup, ExtensionBuilder.createDefaultNpmBuilder(
                 EXTENSION_SOURCE_DIR, EXTENSION_SOURCE_DIR.resolve("dist/cloud-storage-extension.js")));
         exasolTestSetup.getDefaultBucket().uploadFile(ADAPTER_JAR, ADAPTER_JAR.getFileName().toString());
+        client = setup.client();
+        s3setup = S3Setup.create();
+        connection = exasolTestSetup.createConnection();
+        exasolObjectFactory = new ExasolObjectFactory(connection, ExasolObjectConfiguration.builder().build());
+        exasolObjectFactory.createConnectionDefinition(S3_CONNECTION, "", "dummy_user", s3setup.getSecret());
     }
 
     private static Path getAdapterJar() {
@@ -48,15 +67,19 @@ class ExtensionIT {
 
     @AfterAll
     static void teardown() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
         if (setup != null) {
             setup.close();
         }
         exasolTestSetup.getDefaultBucket().deleteFileNonBlocking(ADAPTER_JAR.getFileName().toString());
         exasolTestSetup.close();
+        s3setup.close();
     }
 
     @AfterEach
-    void cleanup() {
+    void cleanup() throws SQLException {
         setup.cleanup();
     }
 
@@ -73,8 +96,85 @@ class ExtensionIT {
     }
 
     @Test
+    void installingWrongVersionFails() {
+        client.assertRequestFails(() -> client.install("wrongVersion"),
+                equalTo("Installing version 'wrongVersion' not supported, try '" + PROJECT_VERSION + "'."),
+                equalTo(400));
+        setup.exasolMetadata().assertNoScripts();
+    }
+
+    @Test
     void installCreatesScripts() {
         setup.client().install();
+        assertScriptsInstalled();
+    }
+
+    @Test
+    void installingTwiceCreatesScripts() {
+        setup.client().install();
+        setup.client().install();
+        assertScriptsInstalled();
+    }
+
+    @Test
+    void exportImportWorksAfterInstallation() throws SQLException {
+        setup.client().install();
+        verfiyExportImportWorks();
+    }
+
+    private void verfiyExportImportWorks() throws SQLException {
+        final ExasolSchema schema = exasolObjectFactory.createSchema("TESTING_SCHEMA_" + System.currentTimeMillis());
+        final String bucket = s3setup.createBucket();
+        try {
+            final Table sourceTable = schema.createTable("SRC_TABLE", "ID", "INTEGER", "NAME", "VARCHAR(10)");
+            final Table targetTable = schema.createTable("TARGET_TABLE", "ID", "INTEGER", "NAME", "VARCHAR(10)");
+            sourceTable.insert(1, "a").insert(2, "b").insert(3, "c");
+            exportIntoS3(sourceTable, bucket, "data");
+            importFromS3IntoExasol(targetTable, bucket, "data/*");
+            try (var statement = connection
+                    .prepareStatement("select * from " + targetTable.getFullyQualifiedName() + " order by 1")) {
+                assertThat(statement.executeQuery(),
+                        table().row(1, "a").row(2, "b").row(3, "c").matches(TypeMatchMode.NO_JAVA_TYPE_CHECK));
+            }
+        } finally {
+            schema.drop();
+            s3setup.deleteBucket(bucket);
+        }
+    }
+
+    private void importFromS3IntoExasol(final Table table, final String bucket, final String file) throws SQLException {
+        executeStatement("IMPORT INTO " + table.getFullyQualifiedName() + "\n" //
+                + "FROM SCRIPT " + ExtensionManagerSetup.EXTENSION_SCHEMA_NAME + ".IMPORT_PATH WITH\n" //
+                + "BUCKET_PATH              = 's3a://" + bucket + "/" + file + "'\n" //
+                + "DATA_FORMAT              = 'PARQUET'\n" //
+                + "S3_ENDPOINT              = '" + s3setup.getS3Endpoint() + "'\n" //
+                + "S3_CHANGE_DETECTION_MODE = 'none'\n" //
+                + "TRUNCATE_STRING          = 'true'\n" //
+                + "CONNECTION_NAME          = '" + S3_CONNECTION + "'\n" //
+                + "PARALLELISM              = 'nproc()'\n");
+    }
+
+    private void exportIntoS3(final Table table, final String bucket, final String s3Path) {
+        executeStatement("EXPORT " + table.getFullyQualifiedName() + "\n" //
+                + "INTO SCRIPT " + ExtensionManagerSetup.EXTENSION_SCHEMA_NAME + ".EXPORT_PATH WITH\n" //
+                + "BUCKET_PATH     = 's3a://" + bucket + "/" + s3Path + "'\n" //
+                + "DATA_FORMAT     = 'PARQUET'\n" //
+                + "S3_ENDPOINT     = '" + s3setup.getS3Endpoint() + "'\n" //
+                + "CONNECTION_NAME = '" + S3_CONNECTION + "'\n" //
+                + "PARALLELISM     = 'iproc()'");
+    }
+
+    private void executeStatement(final String sql) {
+        LOGGER.info(() -> "Running statement '" + sql + "'...");
+        try (var statement = connection.createStatement()) {
+            statement.execute(sql);
+        } catch (final SQLException exception) {
+            throw new IllegalStateException("Failed to execute statement '" + sql + "': " + exception.getMessage(),
+                    exception);
+        }
+    }
+
+    private void assertScriptsInstalled() {
         setup.exasolMetadata().assertScript(table() //
                 .row(setScript("EXPORT_PATH", "com.exasol.cloudetl.scriptclasses.TableExportQueryGenerator")) //
                 .row(setScript("EXPORT_TABLE", "com.exasol.cloudetl.scriptclasses.TableDataExporter")) //
---
+++ src/test/java/com/exasol/cloudetl/extension/S3Setup.java
@@ -0,0 +1,73 @@
+package com.exasol.cloudetl.extension;
+
+import java.util.logging.Logger;
+
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer.Service;
+import org.testcontainers.utility.DockerImageName;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+class S3Setup implements AutoCloseable {
+    private static final Logger LOGGER = Logger.getLogger(S3Setup.class.getName());
+    private final LocalStackContainer container;
+    private final AmazonS3 client;
+
+    public S3Setup(final LocalStackContainer container) {
+        this.container = container;
+        this.client = createS3Client();
+    }
+
+    static S3Setup create() {
+        @SuppressWarnings("resource")
+        final LocalStackContainer container = new LocalStackContainer(
+                DockerImageName.parse("localstack/localstack:1.2.0")).withServices(Service.S3).withReuse(true);
+        container.start();
+        return new S3Setup(container);
+    }
+
+    AmazonS3 createS3Client() {
+        return AmazonS3ClientBuilder.standard().withPathStyleAccessEnabled(true)
+                .withEndpointConfiguration(getS3EndpointConfig())
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(this.container.getAccessKey(), this.container.getSecretKey())))
+                .disableChunkedEncoding().build();
+    }
+
+    AwsClientBuilder.EndpointConfiguration getS3EndpointConfig() {
+        return new AwsClientBuilder.EndpointConfiguration(
+                this.container.getEndpointOverride(LocalStackContainer.Service.S3).toString(),
+                this.container.getRegion());
+    }
+
+    String getS3Endpoint() {
+        final String s3ContainerNetworkGatewayAddress = this.container.getContainerInfo().getNetworkSettings()
+                .getNetworks().values().iterator().next().getGateway();
+        return getS3EndpointConfig().getServiceEndpoint().replaceAll("127.0.0.1", s3ContainerNetworkGatewayAddress);
+    }
+
+    String getSecret() {
+        return "S3_ACCESS_KEY=" + this.container.getAccessKey() + ";S3_SECRET_KEY=" + this.container.getSecretKey();
+    }
+
+    public String createBucket() {
+        final String uniqueBucketName = "testing-bucket-" + System.currentTimeMillis();
+        this.client.createBucket(uniqueBucketName);
+        return uniqueBucketName;
+    }
+
+    public void deleteBucket(final String bucket) {
+        this.client.listObjects(bucket).getObjectSummaries()
+                .forEach(object -> this.client.deleteObject(object.getBucketName(), object.getKey()));
+        this.client.deleteBucket(bucket);
+    }
+
+    @Override
+    public void close() {
+        this.container.close();
+    }
+}
