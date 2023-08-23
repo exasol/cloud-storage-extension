14d53b0f6d05576df2fc98f2eb563e19725f9568
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-08-23 16:57:44 +0200

    Update configuration

---
+++ extension/jest.config.js
@@ -0,0 +1,21 @@
+
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+const config = {
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    errorOnDeprecated: true,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    injectGlobals: false,
+    modulePathIgnorePatterns: ['<rootDir>/dist/'],
+    testMatch: [
+        "**/?(*.)+(spec|test).ts"
+    ],
+};
+
+export default config;
