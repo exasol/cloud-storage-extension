5c23e3472971c690c19c8a030b5bbbc4783ce0bf
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-10-05 16:20:57 +0200

    Fix compile warnings in Java code

--- src/test/java/org/apache/log4j/MDC.java
+++ src/test/java/org/apache/log4j/MDC.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,42 +17,44 @@
 
 package org.apache.log4j;
 
-import org.apache.log4j.helpers.ThreadLocalMap;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Hashtable;
 
+import org.apache.log4j.helpers.ThreadLocalMap;
+
 /**
- * The MDC class is similar to the {@link NDC} class except that it is
- * based on a map instead of a stack. It provides <em>mapped
- * diagnostic contexts</em>. A <em>Mapped Diagnostic Context</em>, or
- * MDC in short, is an instrument for distinguishing interleaved log
- * output from different sources. Log output is typically interleaved
- * when a server handles multiple clients near-simultaneously.
+ * The MDC class is similar to the {@link NDC} class except that it is based on a map instead of a stack. It provides
+ * <em>mapped diagnostic contexts</em>. A <em>Mapped Diagnostic Context</em>, or MDC in short, is an instrument for
+ * distinguishing interleaved log output from different sources. Log output is typically interleaved when a server
+ * handles multiple clients near-simultaneously.
  * <p/>
- * <p><b><em>The MDC is managed on a per thread basis</em></b>. A
- * child thread automatically inherits a <em>copy</em> of the mapped
- * diagnostic context of its parent.
+ * <p>
+ * <b><em>The MDC is managed on a per thread basis</em></b>. A child thread automatically inherits a <em>copy</em> of
+ * the mapped diagnostic context of its parent.
  * <p/>
- * <p>The MDC class requires JDK 1.2 or above. Under JDK 1.1 the MDC
- * will always return empty values but otherwise will not affect or
- * harm your application.</p>
+ * <p>
+ * The MDC class requires JDK 1.2 or above. Under JDK 1.1 the MDC will always return empty values but otherwise will not
+ * affect or harm your application.
+ * </p>
  *
- * <p>Attention: the application is required to clean up. In web applications
- * this can happen with creating a Servlet Filter and overriding the
- * onFilter method like:</p>
+ * <p>
+ * Attention: the application is required to clean up. In web applications this can happen with creating a Servlet
+ * Filter and overriding the onFilter method like:
+ * </p>
  *
  * <pre>
  * try {
- *    MDC.put(myKey);
- *    chain.doFilter(request, response);
+ *     MDC.put(myKey);
+ *     chain.doFilter(request, response);
  * } finally {
- *    MDC.remove(myKey);
+ *     MDC.remove(myKey);
  * }
  * </pre>
  *
- * <p>Please also see: {@link http://logging.apache.org/log4j/1.2/faq.html#mdcmemoryleak}</p>
+ * <p>
+ * Please also see: {@link http://logging.apache.org/log4j/1.2/faq.html#mdcmemoryleak}
+ * </p>
  *
  * @author Ceki G&uuml;lc&uuml;
  * @since 1.2
@@ -75,21 +77,20 @@ public class MDC {
         }
 
         try {
-            removeMethod = ThreadLocal.class.getMethod("remove", null);
-        } catch (NoSuchMethodException e) {
+            removeMethod = ThreadLocal.class.getMethod("remove");
+        } catch (final NoSuchMethodException e) {
             // don't do anything - java prior 1.5
         }
     }
 
     /**
-     * Put a context value (the <code>o</code> parameter) as identified
-     * with the <code>key</code> parameter into the current thread's
-     * context map.
+     * Put a context value (the <code>o</code> parameter) as identified with the <code>key</code> parameter into the
+     * current thread's context map.
      * <p/>
-     * <p>If the current thread does not have a context map it is
-     * created as a side effect.
+     * <p>
+     * If the current thread does not have a context map it is created as a side effect.
      */
-    public static void put(String key, Object o) {
+    public static void put(final String key, final Object o) {
         if (mdc != null) {
             mdc.put0(key, o);
         }
@@ -98,9 +99,10 @@ public class MDC {
     /**
      * Get the context identified by the <code>key</code> parameter.
      * <p/>
-     * <p>This method has no side effects.
+     * <p>
+     * This method has no side effects.
      */
-    public static Object get(String key) {
+    public static Object get(final String key) {
         if (mdc != null) {
             return mdc.get0(key);
         }
@@ -108,21 +110,18 @@ public class MDC {
     }
 
     /**
-     * Remove the the context identified by the <code>key</code>
-     * parameter.
+     * Remove the the context identified by the <code>key</code> parameter.
      */
-    public static void remove(String key) {
+    public static void remove(final String key) {
         if (mdc != null) {
             mdc.remove0(key);
         }
     }
 
-
     /**
-     * Get the current thread's MDC as a hashtable. This method is
-     * intended to be used internally.
+     * Get the current thread's MDC as a hashtable. This method is intended to be used internally.
      */
-    public static Hashtable getContext() {
+    public static Hashtable<?, ?> getContext() {
         if (mdc != null) {
             return mdc.getContext0();
         } else {
@@ -141,25 +140,24 @@ public class MDC {
         }
     }
 
-
-    private void put0(String key, Object o) {
+    private void put0(final String key, final Object o) {
         if (java1 || tlm == null) {
             return;
         } else {
-            Hashtable ht = (Hashtable) ((ThreadLocalMap) tlm).get();
+            Hashtable<String, Object> ht = (Hashtable<String, Object>) ((ThreadLocalMap) tlm).get();
             if (ht == null) {
-                ht = new Hashtable(HT_SIZE);
+                ht = new Hashtable<>(HT_SIZE);
                 ((ThreadLocalMap) tlm).set(ht);
             }
             ht.put(key, o);
         }
     }
 
-    private Object get0(String key) {
+    private Object get0(final String key) {
         if (java1 || tlm == null) {
             return null;
         } else {
-            Hashtable ht = (Hashtable) ((ThreadLocalMap) tlm).get();
+            final Hashtable<?, ?> ht = (Hashtable<?, ?>) ((ThreadLocalMap) tlm).get();
             if (ht != null && key != null) {
                 return ht.get(key);
             } else {
@@ -168,40 +166,40 @@ public class MDC {
         }
     }
 
-    private void remove0(String key) {
+    private void remove0(final String key) {
         if (!java1 && tlm != null) {
-            Hashtable ht = (Hashtable) ((ThreadLocalMap) tlm).get();
+            final Hashtable<?, ?> ht = (Hashtable<?, ?>) ((ThreadLocalMap) tlm).get();
             if (ht != null) {
                 ht.remove(key);
                 // clean up if this was the last key
                 if (ht.isEmpty()) {
-                        clear0();
+                    clear0();
                 }
             }
         }
     }
 
-    private Hashtable getContext0() {
+    private Hashtable<?, ?> getContext0() {
         if (java1 || tlm == null) {
             return null;
         } else {
-            return (Hashtable) ((ThreadLocalMap) tlm).get();
+            return (Hashtable<?, ?>) ((ThreadLocalMap) tlm).get();
         }
     }
 
     private void clear0() {
         if (!java1 && tlm != null) {
-            Hashtable ht = (Hashtable) ((ThreadLocalMap) tlm).get();
+            final Hashtable<?, ?> ht = (Hashtable<?, ?>) ((ThreadLocalMap) tlm).get();
             if (ht != null) {
                 ht.clear();
             }
             if (removeMethod != null) {
                 // java 1.3/1.4 does not have remove - will suffer from a memory leak
                 try {
-                    removeMethod.invoke(tlm, null);
-                } catch (IllegalAccessException e) {
+                    removeMethod.invoke(tlm);
+                } catch (final IllegalAccessException e) {
                     // should not happen
-                } catch (InvocationTargetException e) {
+                } catch (final InvocationTargetException e) {
                     // should not happen
                 }
             }
