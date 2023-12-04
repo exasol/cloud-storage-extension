dd3f4ef6eac63b0d4862d701ada8a3ff79978277
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-12-04 08:07:20 +0100

    Remove unnecessary system property

--- src/test/java/com/exasol/cloudetl/extension/ExtensionIT.java
+++ src/test/java/com/exasol/cloudetl/extension/ExtensionIT.java
@@ -46,9 +46,6 @@ class ExtensionIT extends AbstractScriptExtensionIT {
 
     @BeforeAll
     static void setup() throws FileNotFoundException, BucketAccessException, TimeoutException, SQLException {
-        if (System.getProperty("com.exasol.dockerdb.image") == null) {
-            System.setProperty("com.exasol.dockerdb.image", "8.23.1");
-        }
         exasolTestSetup = new ExasolTestSetupFactory(Paths.get("no-cloud-setup")).getTestSetup();
         ExasolVersionCheck.assumeExasolVersion8(exasolTestSetup);
         setup = ExtensionManagerSetup.create(exasolTestSetup, ExtensionBuilder.createDefaultNpmBuilder(
